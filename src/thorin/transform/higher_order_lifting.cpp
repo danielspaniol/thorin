dcfe24b319e394d2349a1f6d7442fef4b4bd3293
Roland Leißa <leissa@cs.uni-saarland.de>
2017-03-24 04:59:40 +0100

    more tweaking of higher_order_lifting

--- src/thorin/transform/higher_order_lifting.cpp
+++ src/thorin/transform/higher_order_lifting.cpp
@@ -21,27 +21,38 @@ void higher_order_lifting(World& world) {
             if (continuation != scope.entry() && continuation->order() > 1) {
                 DLOG("lift: {}", continuation);
                 Scope cur(continuation);
-                std::vector<const Def*> defs;
-
-                for (auto def : free_defs(cur)) {
-                    if (!def->isa<Continuation>() || !top.contains(def->as_continuation())) {
-                        DLOG("free def: {}", def);
-                        defs.push_back(def);
-                    }
-                }
-
-                Scope lifted(lift(cur, defs));
+                auto free_defs = thorin::free_defs(cur);
 
                 for (auto use : continuation->copy_uses()) {
                     if (auto caller = use->isa_continuation()) {
-                        if (use.index() == 0) {
-                            caller->jump(lifted.entry(), concat(caller->args(), defs), caller->jump_debug());
+                        if (use.index() == 0 && !cur.contains(use)) {
+                            auto fd = free_defs;
+                            for (size_t i = 0; i != caller->num_args(); ++i)
+                                fd.erase(caller->arg(i));
+
+                            std::vector<const Def*> defs;
+                            for (auto def : fd) {
+                                if (!def->isa<Continuation>() || !top.contains(def->as_continuation())) {
+                                    DLOG("free def: {}", def);
+                                    defs.push_back(def);
+                                }
+                            }
+
+                            Array<const Def*> empty(continuation->num_params());
+                            Mangler mangler(cur, empty, defs);
+                            auto lifted = mangler.mangle_head();
+
+                            for (size_t i = 0; i != caller->num_args(); ++i)
+                                mangler.def2def_[caller->arg(i)] = lifted->param(i);
+
+                            mangler.mangle_body();
+                            top.emplace(lifted);
+                            caller->jump(lifted, concat(caller->args(), defs), caller->jump_debug());
                             dirty = true;
                         }
                     }
                 }
 
-                top.insert(lifted.entry());
             }
         }
 
