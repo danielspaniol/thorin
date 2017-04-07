ed96c93db642044283840e3d3b7ae4870ce2c7f1
Roland Leißa <leissa@cs.uni-saarland.de>
2017-04-08 01:34:53 +0200

    only keep return-continuation for top-level continuations

--- src/thorin/transform/lower2cff.cpp
+++ src/thorin/transform/lower2cff.cpp
@@ -18,29 +18,31 @@ void lower2cff(World& world) {
         Scope::for_each(world, [&] (Scope& scope) {
             bool dirty = false;
 
-            auto is_bad = [&] (Continuation* callee) {
-                if (callee->empty())
-                    return false;
-                if (local)
-                    return scope.inner_contains(callee) && !callee->is_basicblock();
-                else {
-                    if (top.contains(callee))
-                        return !callee->is_returning() && !scope.contains(callee);
-                    else
-                        return !callee->is_basicblock();
-                }
-            };
-
             for (auto n : scope.f_cfg().post_order()) {
                 auto continuation = n->continuation();
                 if (auto callee = continuation->callee()->isa_continuation()) {
+                    bool is_top = top.contains(callee);
+
+                    auto is_bad = [&] (Continuation* callee) {
+                        if (callee->empty())
+                            return false;
+                        if (local)
+                            return scope.inner_contains(callee) && !callee->is_basicblock();
+                        else {
+                            if (is_top)
+                                return !callee->is_returning() && !scope.contains(callee);
+                            else
+                                return !callee->is_basicblock();
+                        }
+                    };
+
                     if (!scope.contains(callee) && is_bad(callee)) {
                         DLOG("bad: {}: {} at {}", callee, callee->type(), callee->location());
                         todo = dirty = true;
 
                         Call call(continuation);
                         call.callee() = callee;
-                        // it's fine to keep one higher-order arg - guess the last one
+                        // it's fine to keep one higher-order arg for top-level continuations - guess the last one
                         size_t last = size_t(-1);
                         for (size_t i = 0, e = call.num_args(); i != e; ++i) {
                             if (callee->param(i)->order() > 0) {
@@ -49,7 +51,9 @@ void lower2cff(World& world) {
                             } else
                                 call.arg(i) = nullptr;
                         }
-                        call.arg(last) = nullptr;
+
+                        if (is_top)
+                            call.arg(last) = nullptr;
 
                         const auto& p = cache.emplace(call, nullptr);
                         Continuation*& target = p.first->second;
