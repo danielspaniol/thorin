a9f1cdd7904f72ea62a205efb1dd5e7ff048f3c5
Roland Leißa <leissa@cs.uni-saarland.de>
2017-05-19 01:16:46 +0200

    reworked parameter elimination

--- src/thorin/transform/cleanup_world.cpp
+++ src/thorin/transform/cleanup_world.cpp
@@ -73,47 +73,121 @@ void Cleaner::unreachable_code_elimination() {
     }
 }
 
+static bool is_used_higher_order(Continuation* continuation) {
+    for (auto use : continuation->uses()) {
+        if (use.index() != 0 || !use->isa<Continuation>())
+            return true;
+    }
+
+    return false;
+}
+
 void Cleaner::eliminate_params() {
-    for (auto ocontinuation : world().copy_continuations()) {
-        std::vector<size_t> proxy_idx;
-        std::vector<size_t> param_idx;
+    ParamMap<const Def*> param2def;
 
-        if (!ocontinuation->empty() && !world().is_external(ocontinuation)) {
-            for (auto use : ocontinuation->uses()) {
-                if (use.index() != 0 || !use->isa_continuation())
-                    goto next_continuation;
-            }
+    auto mark_dynamic = [&] (Continuation* continuation) {
+        for (auto param : continuation->params())
+            param2def[param] = param;
+    };
 
-            for (size_t i = 0, e = ocontinuation->num_params(); i != e; ++i) {
-                auto param = ocontinuation->param(i);
-                if (param->num_uses() == 0)
-                    proxy_idx.push_back(i);
-                else
-                    param_idx.push_back(i);
-            }
+    // all external continuations' params are dynamic ...
+    for (auto continuation : world().externals())
+        mark_dynamic(continuation);
 
-            if (!proxy_idx.empty()) {
-                auto ncontinuation = world().continuation(world().fn_type(ocontinuation->type()->ops().cut(proxy_idx)),
-                                            ocontinuation->cc(), ocontinuation->intrinsic(), ocontinuation->debug_history());
-                size_t j = 0;
-                for (auto i : param_idx) {
-                    ocontinuation->param(i)->replace(ncontinuation->param(j));
-                    ncontinuation->param(j++)->debug() = ocontinuation->param(i)->debug_history();
+    // ... and so are all continuations with an empty body or used in a higher-order context
+    for (auto continuation : world().continuations()) {
+        if (continuation->empty() || is_used_higher_order(continuation))
+            mark_dynamic(continuation);
+    }
+
+    // fixed-point iteration to statically find out what a param is
+    for (bool todo = true; todo;) {
+        todo = false;
+
+        for (auto continuation : world().continuations()) {
+            if (auto callee = continuation->callee()->isa_continuation()) {
+                for (size_t i = 0, e = callee->num_params(); i != e; ++i) {
+                    auto def = continuation->arg(i);
+
+                    // if it's a param - find its representative in param2def
+                    if (auto param = def->isa<Param>()) {
+                        auto i = param2def.find(param);
+                        if (i == param2def.end())
+                            continue;       // not handled yet - wait to next iteration
+                        def = i->second;
+                    }
+
+                    // try to put it into the map
+                    auto p = param2def.emplace(callee->param(i), def);
+                    if (p.second)
+                        todo = true;        // first entry
+                    else {
+                        auto i = p.first;   // could not insert - we already map to sth
+                        if (def != i->second && i->second != i->first) {
+                            i->second = i->first;
+                            todo = true;
+                        }
+                    }
                 }
-
-                ncontinuation->jump(ocontinuation->callee(), ocontinuation->args(), ocontinuation->jump_debug());
-                ocontinuation->destroy_body();
-
-                for (auto use : ocontinuation->copy_uses()) {
-                    auto ucontinuation = use->as_continuation();
-                    assert(use.index() == 0);
-                    ucontinuation->jump(ncontinuation, ucontinuation->args().cut(proxy_idx), ucontinuation->jump_debug());
-                }
-
-                todo_ = true;
             }
         }
-next_continuation:;
+    }
+
+    // apply analysis info
+    for (auto ocontinuation : world().copy_continuations()) {
+        std::vector<size_t> proxy_idx, param_idx;
+        bool is_higher_order = false;
+
+        for (size_t i = 0, e = ocontinuation->num_params(); i != e; ++i) {
+            auto param = ocontinuation->param(i);
+            auto it = param2def.find(param);
+            if (it == param2def.end()) {
+                DLOG("dead: {}", param);
+                proxy_idx.push_back(i);     // if param unused or maps to some unique value
+            } else {
+                is_higher_order |= param->order() > 0;
+                auto mapped = it->second;
+                if (param != mapped) {
+                    DLOG("{} -> {}", param, mapped);
+                    proxy_idx.push_back(i);
+                } else
+                    param_idx.push_back(i); // do nothing otherwise
+            }
+        }
+
+        if (is_higher_order)
+            continue; // don't do sth here: we might get probls due to lower2cff
+
+        if (!proxy_idx.empty()) {
+            auto ncontinuation = world().continuation(world().fn_type(ocontinuation->type()->ops().cut(proxy_idx)),
+                    ocontinuation->cc(), ocontinuation->intrinsic(), ocontinuation->debug_history());
+
+            // replace old params which must be kept with ncontinuation's ones
+            size_t j = 0;
+            for (auto i : param_idx) {
+                ocontinuation->param(i)->replace(ncontinuation->param(j));
+                ncontinuation->param(j++)->debug() = ocontinuation->param(i)->debug_history();
+            }
+
+            // replace old params with its according analysis info or do nothing if it is dead
+            for (auto i : proxy_idx) {
+                auto it = param2def.find(ocontinuation->param(i));
+                if (it != param2def.end())
+                    ocontinuation->param(i)->replace(it->second);
+            }
+
+            // wire ncontinuation into the program
+            ncontinuation->jump(ocontinuation->callee(), ocontinuation->args(), ocontinuation->jump_debug());
+            ocontinuation->destroy_body();
+
+            for (auto use : ocontinuation->copy_uses()) {
+                auto ucontinuation = use->as_continuation();
+                assert(use.index() == 0);
+                ucontinuation->jump(ncontinuation, ucontinuation->args().cut(proxy_idx), ucontinuation->jump_debug());
+            }
+
+            todo_ = true;
+        }
     }
 }
 
